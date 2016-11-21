badb772db4f69ac84d524772f90e63c1dfb578c4
Andy Hudson <andy.hudson@kaonix.com>
2016-11-21 09:21:01 +0000

    Finished Chapter 10.

--- test/SportsStore.Tests/OrderControllerTests.cs
+++ test/SportsStore.Tests/OrderControllerTests.cs
@@ -29,5 +29,43 @@ namespace SportsStore.Tests
             Assert.True(string.IsNullOrEmpty(result.ViewName));
             Assert.False(result.ViewData.ModelState.IsValid);
         }
+
+        [Fact]
+        public void CannotCheckoutInvalidShippingDetails()
+        {
+            // arrange
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+
+            OrderController controller = new OrderController(mock.Object, cart);
+            controller.ModelState.AddModelError("error", "error");
+
+            // act
+            ViewResult result = controller.Checkout(new Order()) as ViewResult;
+
+            // assert
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        public void CanCheckoutAndSubmitOrder()
+        {
+            // arrange
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            OrderController controller = new OrderController(mock.Object, cart);
+
+            // act 
+            RedirectToActionResult result = controller.Checkout(new Order()) as RedirectToActionResult;
+
+            // assert
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Once);
+            Assert.Equal("Completed", result.ActionName);
+        }
     }
 }
